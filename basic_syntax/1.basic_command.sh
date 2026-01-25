37e2d13e6b2848603627a8334b2ce1d1a82407a6
daehanzz <qdpq02@naver.com>
2026-01-25 19:03:42 +0900

    title1
    title1의 수정사항은 ... 입니다.

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -1,2 +1,8 @@
 # git add . 은 모든변경(추가)사항을 staging area로 이동
 git add .
+
+# git commit으 ㄴ staging area의 변경사항을 확정짓고 commitId 생성
+git commit -m '메세지'
+
+# 원격 repository에 main 브랜치 변경사항(commitId)을 업로드
+git push origin main
