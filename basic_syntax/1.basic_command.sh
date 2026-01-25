55919d4b71ddb8afb96dfa1d109bfe89c3eca3f1
daehanzz <qdpq02@naver.com>
2026-01-25 19:21:41 +0900

    add와 commit을 동시에

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -6,3 +6,16 @@ git commit -m '메세지'
 
 # 원격 repository에 main 브랜치 변경사항(commitId)을 업로드
 git push origin main
+
+
+# local repo의 commit 이력 조회
+git log
+
+# git 로그를 간결하게 조회
+git log --oneline
+
+# git 로그를 그래프형태로 조회
+git log --graph
+
+# main 브랜치 뿐만 아니라 모든 전체 commit 이력을 조회
+git log --all
